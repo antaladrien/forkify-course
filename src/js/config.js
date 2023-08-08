681a8efc085df7d55738d58511824fc21e299519
antaladrien <adrien.kardics@gmail.com>
2023-08-08 12:25:29 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'dd40f918-9f84-454b-a388-84a41c7d7e0a';
+export const MODAL_CLOSE_SEC = 2.5;
